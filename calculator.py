2765fd34792d56e4f178b1f3884f19f9b9a382a7
Iuri Torres <iuri.t1000@gmail.com>
2023-04-07 02:37:02 -0300

    added: basic operations

---
+++ calculator.py
@@ -0,0 +1,11 @@
+class Calculator:
+    
+    # basic operations
+    def add      (num1: int|float, num2: int|float) -> int|float : return (num1 + num2)
+    def subtract (num1: int|float, num2: int|float) -> int|float : return (num1 - num2)
+    def multiply (num1: int|float, num2: int|float) -> int|float : return (num1 * num2)
+    def divide   (num1: int|float, num2: int|float) -> int|float : return (num1 / num2)
+
+
+if __name__ == '__main__':
+    pass
