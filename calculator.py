83bf11400b856fde84b836a929f001107330ef93
Iuri Torres <iuri.t1000@gmail.com>
2023-04-07 02:51:44 -0300

    added: product() method

--- calculator.py
+++ calculator.py
@@ -6,6 +6,21 @@ class Calculator:
     def multiply (num1: int|float, num2: int|float) -> int|float : return (num1 * num2)
     def divide   (num1: int|float, num2: int|float) -> int|float : return (num1 / num2)
 
+    # product of a list of numbers
+    def product(array: list) -> int|float:
+        result = array[0]
+
+        for index in range(len(array)):
+            if (index + 1) == len(array):
+                break
+
+            else:
+                result *= array[index + 1]
+
+        return result
+
+
 
 if __name__ == '__main__':
-    pass
+    prod = Calculator.product([3, 3, 3])
+    print(prod)
