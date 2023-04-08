ec01597e5386dd250936d48e0225ba5b1407b09f
Iuri Torres <iuri.t1000@gmail.com>
2023-04-08 01:53:04 -0300

    finished auto deploy setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open('README.md', 'r') as file:
+    readme = file.read()
+
+setup(
+    name='wiu',
+    version='0.0.1',
+    license='MIT License',
+    author='Iuri Torres & João Witor',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='iuri.t1000@gmail.com',
+    keywords='wiu',
+    description=u'A library made to practice and improve programming and english skills during our university.',
+    packages=['wiu'],
+    # install_requires=[''],
+)
